8627474227e32a53ad3c71110c985c712072ad2a
alas1413 <92256695+alas1413@users.noreply.github.com>
2021-10-27 09:36:14 +0530

    Add files via upload

---
+++ listofpositive.py
@@ -0,0 +1,11 @@
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 = [8, -21, 9, 45, 55, -93]
+
+# iterating each number in list
+for num in list1:
+
+    # checking condition
+    if num >= 0:
+        print(num, end=" ")
